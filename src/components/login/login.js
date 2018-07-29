db54091a635ee9ee9c4a81bde9611cbc9602c87a
nghianh993 <nghianh993@gmail.com>
2018-07-29 13:20:15 +0700

    update validate

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,5 +1,20 @@
 import React, { Component } from 'react';
-import { Form, Button, FormGroup, Label, Input, Col } from 'reactstrap';
+import { Button, FormGroup, Label, Col } from 'reactstrap';
+import { isEmpty } from 'validator';
+import Form from 'react-validation/build/form';
+import Input from 'react-validation/build/input';
+
+const required = (value) => {
+    if (isEmpty(value)) {
+        return <label className="form-text text-danger" style={{padding: "5px"}}>Tên đăng nhập không được để trống</label>;
+    }
+}
+
+const minLength = (value) => {
+    if (value.trim().length < 6) {
+        return <label className="form-text text-danger" style={{padding: "5px"}}>Mật khẩu không được nhỏ hơn 6 kí tự</label>;
+    }
+}
 
 class Login extends Component {
 
@@ -12,21 +27,45 @@ class Login extends Component {
         };
     }
 
-    handleSubmit = (event, errors, values) => {
-        this.setState({errors, values});
+    // handleChange = event => {
+    //     const target = event.target;
+    //     const value = target.type === 'checkbox' ? target.checked : target.value;
+    //     const name = target.name;
+    //     this.setState({
+    //         [name]: value
+    //     });
+    // };
+
+    onSubmit(e){
+        e.preventDefault();
+        if(this.form.validateAll()){
+            alert("success");
+        }
+
     }
 
     render() {
         return (
-            <Form className="form-horizontal m-t-20">
+            <Form className="form-horizontal m-t-20" onSubmit={e => this.onSubmit(e)} ref={c => { this.form = c }}> 
                 <FormGroup>
                     <Col xs="12">
-                        <Input className="form-control input-lg" name="username" type="text" placeholder="Tên đăng nhập" />
+                        <Input 
+                            className="form-control input-lg" 
+                            name="username" type="text" 
+                            placeholder="Tên đăng nhập" 
+                            validations={[required]}
+                        />
                     </Col>
                 </FormGroup>
                 <FormGroup>
                     <Col xs="12">
-                        <Input className="form-control input-lg" name="password" type="password" placeholder="Mật khẩu" />                        
+                        <Input 
+                            className="form-control input-lg" 
+                            name="password" 
+                            type="password" 
+                            placeholder="Mật khẩu" 
+                            validations={[required, minLength]}
+                        />                        
                     </Col>
                 </FormGroup>
                 <FormGroup>
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'http://10.15.152.77:9000';
